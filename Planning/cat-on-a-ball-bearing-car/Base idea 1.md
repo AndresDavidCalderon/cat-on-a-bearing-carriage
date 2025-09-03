2f4c77e773d03b3f282d8344a869a578b8dea8b3
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-09-02 21:56:00 -0500

    basic movement

--- Planning/cat-on-a-ball-bearing-car/Base idea 1.md
+++ Planning/cat-on-a-ball-bearing-car/Base idea 1.md
@@ -3,4 +3,11 @@ A good concept, is that ball bearing cars are normally only propelled by gravity
 
 ## About the map.
 
-Yes it can be a grid in order to be generated easily, however, there may be bigger tiles (in whole numbers), which can be probably generated and fitted easily. This makes it so that there may be roundabouts, 
+Yes it can be a grid in order to be generated easily, however, there may be bigger tiles (in whole numbers), which can be probably generated and fitted easily. This makes it so that there may be roundabouts, complex plazas with obstacles, and maybe even parked cars.
+
+## About the car
+
+The car should be finicky to drive but feel good. So i'm thinking of how one moves the shopping car when one is a kid, when turning, the car moves its back wheels sideways too, and this makes for a satisfying "drift" like effect. This should be easier than a normal turn, however it should be exaggerated, with the back moving rapidly so that it looks intentional.
+
+## About the gameplay
+You drive the car with the intention to deliver packages to different locations you should see with some sort of crazy taxi-like signaling, and should have a counter of impulse, with more impulse, you should go slightly faster (For dramatic effect), but not too fast or you would be too busy hitting things.
---
+++ player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+enum State{
+	SLIDING,
+	DRIFTING
+}
+
+enum Rotation{
+	Negative,
+	Positive
+}
+
+var current_state:State=State.SLIDING
+@export var drift_point_offset:Vector2
+@export var drift_movement:Vector2=Vector2(50,0)
+
+# Spent when moving. Should be measured in pixels of distance.
+var impulse:float=100
+# Spends impulse.
+var speed:float=impulse
+@export var steering_speed:float=1
+
+var pinpoint:Vector2 #Should be set when starting a drift.
+var drift_direction:Rotation
+
+func _process(delta: float) -> void:
+	velocity=Vector2(0,-speed).rotated(rotation)
+	move_and_slide()
+	
+	var circumstancial_steering_speed=steering_speed*delta
+	if current_state==State.DRIFTING:
+		circumstancial_steering_speed*=5
+	
+	if Input.is_action_pressed("SteerLeft"):
+		rotation-=steering_speed*delta
+	if Input.is_action_pressed("SteerRight"):
+		rotation+=steering_speed*delta
+	
+	if Input.is_action_just_pressed("drift"):
+		set_state(State.DRIFTING)
+		pinpoint=position+drift_point_offset.rotated(rotation)
+	if Input.is_action_just_released("drift"):
+		set_state(State.SLIDING)
+		if Input.is_action_pressed("SteerLeft"):
+			drift_direction=Rotation.Negative
+		if Input.is_action_pressed("SteerRight"):
+			drift_direction=Rotation.Positive
+	
+	if current_state==State.DRIFTING:
+		position=pinpoint-drift_point_offset.rotated(rotation)
+		pinpoint+=drift_movement.rotated(rotation)*delta*(1 if drift_direction==Rotation.Positive else -1)
+func set_state(new_state:State):
+	current_state=new_state
