eaaeb82bf74a43f33419c9328bbc7f92292b907a
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-09-03 13:27:25 -0500

    compass

---
+++ UI/compass.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var target_provider=get_node("/root/World")
+@onready var player=get_node("/root/World/Player") as Node2D
+
+func _process(delta: float) -> void:
+	if target_provider.current_target!=null:
+		var target=target_provider.current_target.position as Vector2
+		$Nail.rotation= target.angle_to_point(player.position)-player.rotation
