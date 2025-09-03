6a589d3dc4cb1e3f1bf58188fea40d3f7cb47bb6
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-09-03 09:27:21 -0500

    set first delivery target

--- world.gd
+++ world.gd
@@ -4,5 +4,12 @@ var delivery_targets=[]
 
 var current_target:Node=null
 
+func _ready() -> void:
+	randomize()
+
 func register_target(target:Node):
 	delivery_targets.append(target)
+
+
+func _on_start_pressed() -> void:
+	current_target=delivery_targets.pick_random()
