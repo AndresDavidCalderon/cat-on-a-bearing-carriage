6bb85617837ce833770f0afaa28ba9eb4c105994
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-09-09 15:34:17 -0500

    interior floors

---
+++ world-and-tiles/small_open_roof.gd
@@ -0,0 +1,18 @@
+extends TileMapLayer
+
+@onready var player = get_node("/root/World/Player")
+
+var interior_opacity=0.1
+var default_opacity=0.9
+var current_tween:Tween
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body==player:
+		current_tween=create_tween()
+		current_tween.tween_property(self,"modulate:a",0.1,0.3)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body==player:
+		current_tween=create_tween()
+		current_tween.tween_property(self,"modulate:a",default_opacity,0.3)
