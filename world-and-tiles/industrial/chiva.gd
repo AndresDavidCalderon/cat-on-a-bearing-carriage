c4c66b02ec7ea98bc60cc126782d0db88f93cd7f
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-09-28 21:48:09 -0500

    civil navigation layer

--- world-and-tiles/industrial/chiva.gd
+++ world-and-tiles/industrial/chiva.gd
@@ -1,16 +1,24 @@
 extends AnimatableBody2D
 
+signal path_ended
+
 var speed=400
 var target_idx=0
 var margin=0
-var rotation_speed=3
+var rotation_speed=4
 var acceptable_distance=10
 var pathing=true
 
 @export var path:Node
 
+## If not empty, replaces path with a random one of here.
+@export var possible_paths:Array[Node]
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	if possible_paths.size()>0:
+		path=possible_paths.pick_random()
+
+func _process(delta: float) -> void:
 	if path.get_child_count()<=target_idx or not pathing:
 		return
 	var target_position=path.get_child(target_idx).global_position
@@ -26,3 +34,4 @@ func done_with_target():
 		target_idx+=1
 	else:
 		pathing=false
+		path_ended.emit()
