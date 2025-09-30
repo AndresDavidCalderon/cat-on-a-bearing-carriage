e6d23f1d29df0516e394ae5cb63e0c464df8f439
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-09-29 21:07:36 -0500

    finish give way behaviour
    There should be a mechanism when the target is already ocuppied to actively go somewhere else.

--- world-and-tiles/people/pedestrian.gd
+++ world-and-tiles/people/pedestrian.gd
@@ -12,9 +12,22 @@ var current_state=State.GOING
 
 
 var speed=200
+var give_way_speed=50
 var rotation_speed=20
 var last_delta:float
-var give_way:bool
+var give_way_angle:float=0
+var angle_give_way_offset_p_second:float=0
+
+## Can be managed by another pedestrian if they find the intersection first.
+## they must change it back to true after finding we are no longer in their way.
+var give_way:bool=true
+
+## Useful for managing the other pedestrian's give way state.
+var giving_way_to:Pedestrian
+
+## Latest velocity used.
+var move_intention:Vector2
+
 
 func _ready() -> void:
 	set_new_target()
@@ -24,15 +37,36 @@ func _process(delta: float) -> void:
 	last_delta=delta
 	if $NavigationAgent2D.is_navigation_finished() or match_provider.current_match_state!=match_provider.matchState.PLAYING:
 		return
+	
 	match current_state:
 		State.GOING:
 			var target=$NavigationAgent2D.get_next_path_position()
 			var new_velocity=global_position.direction_to(target)*speed*delta
+			move_intention=new_velocity
+			if give_way:
+				for i in $WayArea.get_overlapping_bodies():
+					if i is Pedestrian:
+						i.give_way=false
+						current_state=State.GIVING_WAY
+						giving_way_to=i
+						give_way_angle=i.move_intention.angle()+PI/2
+						break
 			if $NavigationAgent2D.avoidance_enabled:
 				$NavigationAgent2D.set_velocity(new_velocity)
 			else:
 				_on_navigation_agent_2d_velocity_computed(new_velocity)
-
+			
+			give_way_angle=rotate_toward(give_way_angle,0,angle_give_way_offset_p_second*delta)
+			
+		State.GIVING_WAY:
+			give_way_angle+=angle_give_way_offset_p_second*delta
+			var give_way_vector=Vector2(0,1).rotated(give_way_angle)*give_way_speed*delta
+			move_and_collide(give_way_vector)
+			move_intention=give_way_vector
+			if not pedestrians_in_area().has(giving_way_to):
+				giving_way_to.give_way=true
+				current_state=State.GOING
+	
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 	move_and_collide(safe_velocity)
 	rotation=rotate_toward(rotation,safe_velocity.angle()+PI/2,rotation_speed*last_delta)
@@ -44,6 +78,15 @@ func _on_navigation_agent_2d_navigation_finished() -> void:
 func set_new_target():
 	$NavigationAgent2D.target_position=get_parent().get_node("InterestPoints").get_children().pick_random().global_position
 
+func pedestrians_in_area():
+	var pedestrians=[]
+	for i in $WayArea.get_overlapping_bodies():
+		if i is Pedestrian:
+			pedestrians.append(i)
+	return pedestrians
+
+func is_pedestrian(a):
+	return a is Pedestrian
 
 func _on_attention_span_timeout() -> void:
 	set_new_target()
