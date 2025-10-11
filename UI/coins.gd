7449808b47b21e16061ae87ad70ee65b24820e46
Andres David Calderon <andresdavidcalderonjimenez@gmail.com>
2025-10-11 17:06:55 -0500

    implement coin gain sound

--- UI/coins.gd
+++ UI/coins.gd
@@ -5,6 +5,8 @@ var last_amount:int
 var hide_speed=0.5
 var time_since_effect=0
 var min_time_since_effect=1
+var time_since_coin_gain=0
+var only_play_gain_after=1
 
 func _ready() -> void:
 	update(false)
@@ -16,7 +18,7 @@ func _process(delta: float) -> void:
 	$WinEffect.modulate.a=clamp($WinEffect.modulate.a-hide_speed*delta,0,1)
 	if not effect_shown:
 		time_since_effect+=delta
-
+	time_since_coin_gain+=delta
 
 func update(effect=true):
 	$HBoxContainer/Label.text=str(GlobalScore.coins)
@@ -27,12 +29,16 @@ func update(effect=true):
 			$WinEffect/Stars2.show()
 			$WinEffect/Label.text="+"+str(gained)
 			$WinEffect/Label.add_theme_color_override("font_color",Color("fff300"))
+			if time_since_coin_gain>only_play_gain_after:
+				$CoinGain.play()
+			time_since_coin_gain=0
 		else:
 			$WinEffect/Stars.hide()
 			$WinEffect/Stars2.hide()
 			$WinEffect/Label.text=str(gained)
 			$WinEffect/Label.add_theme_color_override("font_color",Color("80303a"))
-			
+			$CoinSpend.play()
+		
 		last_amount=GlobalScore.coins
 		$WinEffect.modulate.a=1
 		if time_since_effect>min_time_since_effect and not effect_shown:
